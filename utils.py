0f02624ddb7fdbed8af4c63b63c536e14362103f
Till Meyer zu Westram <till@meyerzuwestram.net>
2019-12-04 15:52:50 +0100

    detect dirty git tree

--- utils.py
+++ utils.py
@@ -7,9 +7,28 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-def git_hash(directory=None):
+def git_hash(check_dirty=True, directory=None):
     try:
-        return subprocess.check_output(["git", "--git-dir=%s/.git" % (directory or "."), "rev-parse", "--short", "HEAD"]).strip().decode("ascii")
+        sha = subprocess.check_output([
+            "git", 
+            "--git-dir=%s/.git" % (directory or "."), 
+            "rev-parse", 
+            "--short", 
+            "HEAD"]).strip().decode("ascii")
+        if not check_dirty:
+            return sha
+        else:
+            # check for uncommitted changes
+            retval = subprocess.call([
+                "git", 
+                "--git-dir=%s/.git" % (directory or "."), 
+                "diff-index", 
+                "--quiet", 
+                "HEAD", 
+                "--"])
+            is_dirty = retval > 0
+            is_dirty_str = "DIRTY-" if is_dirty else ""
+            return "%s%s" % (is_dirty_str, sha)
     except subprocess.CalledProcessError as e:
         logger.warning(e)
         return None
