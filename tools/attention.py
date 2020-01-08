604f847b2b56be3e22a97101fb195f5db50ad2f1
Till Meyer zu Westram <till@meyerzuwestram.net>
2020-01-08 08:45:47 +0100

    fix syntax error in case of training without validation

--- tools/attention.py
+++ tools/attention.py
@@ -23,26 +23,16 @@ else:
 from misc_functions import (get_example_params,
                             preprocess_image,
                             convert_to_grayscale,
-                            save_gradient_images)
+                            save_gradient_images, 
+                            format_np_output)
 from gradcam import GradCam
 from guided_backprop import GuidedBackprop
+from guided_gradcam import guided_grad_cam
 
 
 """ END IMPORTS """
 
 logger = logging.getLogger(__name__)
-
-
-class AttentionNet(DRDNet):
-    from torch.functional import F
-
-    def __init__(self):
-        super().__init__()
-        self.features = self.resnet
-        self.classifier = self.__classifier
-
-    def __classifier(self, x):
-        return F.log_softmax(x, dim=1)
 
 
 if __name__ == '__main__':
@@ -53,7 +43,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-o', '--output', help="Output image name", default=None)
     parser.add_argument('-s', '--state', help="Model state file to load")
-    parser.add_argument('-l', '--layer', type=int, default=1)
+    parser.add_argument('-l', '--layer', type=int, default=-1)
     # `class` is a reserved keyword
     parser.add_argument('-c', '--class', type=int, default=None, help="Class of input image", dest="target_class")
     parser.add_argument('input', help="Input image")
@@ -74,26 +64,44 @@ if __name__ == '__main__':
 
     logger.info("Generating activation map of %s: %s" % (args.input, args.output))
 
-    model = AttentionNet()
+    model = DRDNet()
     model.load_state_dict(torch.load(args.state, map_location=torch.device('cpu')))
 
     orig_image = Image.open(args.input).convert("RGB")
     prep_img = preprocess_image(orig_image)
-    # Grad cam
-    gcv2 = GradCam(model, target_layer=args.layer)
-    # Generate cam mask
-    cam = gcv2.generate_cam(prep_img, args.target_class)
-    logger.info('Grad cam completed')
 
-    # Guided backprop
-    GBP = GuidedBackprop(model)
-    # Get gradients
-    guided_grads = GBP.generate_gradients(prep_img, args.target_class)
-    logger.info('Guided backpropagation completed')
+    if args.layer == -1:
+        cams = (GradCam(model, target_layer=i) for i in range(len(model.features._modules)))
+    else:
+        cams = (GradCam(model, target_layer=args.layer), )
 
-    # Guided Grad cam
-    cam_gb = guided_grad_cam(cam, guided_grads)
-    #save_gradient_images(cam_gb, file_name_to_export + '_GGrad_Cam')
-    grayscale_cam_gb = convert_to_grayscale(cam_gb)
-    save_gradient_images(grayscale_cam_gb, args.output)
-    logger.info('Guided grad cam completed')
+    for i, gcv2 in enumerate(cams):
+        try:
+            # Generate cam mask
+            cam = gcv2.generate_cam(prep_img, args.target_class)
+
+            # Guided backprop
+            GBP = GuidedBackprop(model)
+            # Get gradients
+            guided_grads = GBP.generate_gradients(prep_img, args.target_class)
+
+            # Guided Grad cam
+            cam_gb = guided_grad_cam(cam.T, guided_grads)
+            #save_gradient_images(cam_gb, file_name_to_export + '_GGrad_Cam')
+            gradient = convert_to_grayscale(cam_gb)
+
+            gradient = gradient - gradient.min()
+            gradient /= gradient.max()
+            im = format_np_output(gradient)
+            im = Image.fromarray(im)
+
+            if args.layer == -1:
+                e = args.output.split(".")
+                output = "%s_%02d.%s" % (".".join(e[:-1]), i, e[-1])
+            else:
+                output = args.output 
+
+            im.save(output)
+            logger.info("GradCam for layer %d saved in %s" % (i, output))
+        except Exception as e:
+            logger.error("An error occured calculating the gradcam in layer %d: %s" % (i, e))
